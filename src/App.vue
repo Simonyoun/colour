9829fb70fd0e2f44f8f9d9b26471e6b4293effb5
liwei <2275712059@qq.com>
2020-06-15 22:34:15 +0800

    更新

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,45 @@
 <template>
-  <div id="app"></div>
+  <div id="app" ref="app">
+     <div class="logo">
+        webcolor
+     </div>
+     <div class="body"></div>
+  </div>
 </template>
+<script>
+export default {
+    mounted() {
+        var w = document.documentElement.clientWidth || document.body.clientWidth
+        this.$refs.app.style.white = w + 'px'
+    }
+}
+</script>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  position: relative;
+  .logo {
+    width: 100%;
+    height: 400px;
+    background-color: #000000;
+    color: cornsilk;
+    font-size: 120px;
+    line-height: 170px;
+    font-family: Arial,Helvetica,sans-serif;     
+    text-align: center;
+  }
+  .body {
+    position: absolute;
+    top: 100px;
+    left: 10%;
+    width: 80%;
+    height: 1000px;
+    background-color: aquamarine;
+    opacity: 0.6;
+  }
 }
 </style>
